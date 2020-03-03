24c394e9759d412ec79be85bea1149004aa9e295
Mert Yumer <mertyumer66@gmail.com>
2020-03-03 17:12:45 +0200

    Added Login, Register, Profile and About views

---
+++ Web/TripFinder.Web/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿namespace TripFinder.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class UsersController : BaseController
+    {
+        public IActionResult Login()
+        {
+            return this.View();
+        }
+
+        public IActionResult Register()
+        {
+            return this.View();
+        }
+
+        public IActionResult Profile()
+        {
+            return this.View();
+        }
+    }
+}
