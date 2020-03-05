5574fca850b9944ce1e661911e204bcd1580da0d
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2020-03-05 17:33:12 +0200

    Edited Register.cshtml and Login.cshtml

--- Web/TripFinder.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Web/TripFinder.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
 namespace TripFinder.Web.Areas.Identity.Pages.Account
 {
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
     public class RegisterModel : PageModel
     {
         public void OnGet()
