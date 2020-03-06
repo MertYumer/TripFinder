805c9ab64456abe5f3baa12e237045be2c772078
Mert Yumer <mertyumer66@gmail.com>
2020-03-06 06:10:20 +0200

    Implemented Register, Login and Logout

---
+++ Web/TripFinder.Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,45 @@
+﻿namespace TripFinder.Web.Areas.Identity.Pages.Account
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Logging;
+    using TripFinder.Data.Models;
+
+    [AllowAnonymous]
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly ILogger<LogoutModel> logger;
+
+        public LogoutModel(SignInManager<ApplicationUser> signInManager, ILogger<LogoutModel> logger)
+        {
+            this.signInManager = signInManager;
+            this.logger = logger;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            await this.signInManager.SignOutAsync();
+            this.logger.LogInformation("User logged out.");
+            return this.Redirect("/");
+        }
+
+        public async Task<IActionResult> OnPost(string returnUrl = null)
+        {
+            await this.signInManager.SignOutAsync();
+            this.logger.LogInformation("User logged out.");
+            if (returnUrl != null)
+            {
+                return this.LocalRedirect(returnUrl);
+            }
+            else
+            {
+                return this.RedirectToPage();
+            }
+        }
+    }
+}
