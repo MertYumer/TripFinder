570afbcf0a5024fbfd1a4c574ad9658bbed3b051
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2020-03-18 17:43:50 +0200

    Changed content of About.cshtml

--- Data/TripFinder.Data/Migrations/20200315022047_ChangeCarTypeColumn.cs
+++ Data/TripFinder.Data/Migrations/20200315022047_ChangeCarTypeColumn.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
-
-namespace TripFinder.Data.Migrations
+﻿namespace TripFinder.Data.Migrations
 {
+    using Microsoft.EntityFrameworkCore.Migrations;
+
     public partial class ChangeCarTypeColumn : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
