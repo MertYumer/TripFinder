32f6d056683234109c913d84527304e37c26fbb7
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2020-03-23 10:19:02 +0200

    Added CarsController and CarCreateInputModel

---
+++ Web/TripFinder.Web.ViewModels/Cars/CarCreateInputModel.cs
@@ -0,0 +1,49 @@
+﻿namespace TripFinder.Web.ViewModels.Cars
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using TripFinder.Data.Models;
+    using TripFinder.Services.Mapping;
+
+    public class CarCreateInputModel : IMapTo<Car>
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string Make { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string Model { get; set; }
+
+        public CarType Type { get; set; }
+
+        public string Color { get; set; }
+
+        public int? Year { get; set; }
+
+        public string Image { get; set; }
+
+        [Required]
+        public int PassengerSeats { get; set; }
+
+        [Required]
+        public bool AllowedSmoking { get; set; }
+
+        [Required]
+        public bool AllowedFood { get; set; }
+
+        [Required]
+        public bool AllowedDrinks { get; set; }
+
+        [Required]
+        public bool PlaceForLuggage { get; set; }
+
+        [Required]
+        public bool AllowedPets { get; set; }
+
+        [Required]
+        public bool HasAirConditioning { get; set; }
+    }
+}
---
+++ Web/TripFinder.Web/Controllers/CarsController.cs
@@ -0,0 +1,31 @@
+﻿namespace TripFinder.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using TripFinder.Data.Models;
+
+    [Authorize]
+    public class CarsController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public CarsController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        //[HttpPost]
+        //public async Task<IActionResult> Create()
+        //{ 
+        
+        //}
+    }
+}
