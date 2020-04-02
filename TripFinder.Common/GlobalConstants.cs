47563ae316276d8561e645d22204f53457815e48
Mert Yumer <mertyumer66@gmail.com>
2020-04-02 18:30:16 +0300

    Added privacy policy

--- TripFinder.Common/GlobalConstants.cs
+++ TripFinder.Common/GlobalConstants.cs
@@ -4,6 +4,10 @@
     {
         public const string SystemName = "TripFinder";
 
+        public const string SystemEmail = "tripfinder@gmail.com";
+
+        public const string OwnerName = "Mert Yumer";
+
         public const string AdministratorRoleName = "Administrator";
     }
 }
