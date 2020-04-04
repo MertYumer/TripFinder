a61c9c44c7a28a032b1ae5a162e58403964fa116
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2020-04-04 17:51:50 +0300

    Added timepicker plugin

--- Data/TripFinder.Data/Migrations/20200404115213_AddTripOriginAndDestinationColumn.cs
+++ Data/TripFinder.Data/Migrations/20200404115213_AddTripOriginAndDestinationColumn.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
-
-namespace TripFinder.Data.Migrations
+﻿namespace TripFinder.Data.Migrations
 {
+    using Microsoft.EntityFrameworkCore.Migrations;
+
     public partial class AddTripOriginAndDestinationColumn : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
--- TripFinder.Common/GlobalConstants.cs
+++ TripFinder.Common/GlobalConstants.cs
@@ -3,11 +3,12 @@
     public static class GlobalConstants
     {
         public const string SystemName = "TripFinder";
-
         public const string SystemEmail = "tripfinder@gmail.com";
-
         public const string OwnerName = "Mert Yumer";
 
         public const string AdministratorRoleName = "Administrator";
+
+        public const string DefaultDateFormat = "dd.MM.yyyy";
+        public const string DefaultTimeFormat = "HH:mm";
     }
 }
