526718f98673ea062168fa88261972414a06c3cb
Mert Yumer <mertyumer66@gmail.com>
2020-10-13 15:06:55 +0300

    Changed signing up error message

--- Web/TripFinder.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Web/TripFinder.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -59,7 +59,7 @@ namespace TripFinder.Web.Areas.Identity.Pages.Account
             public string PhoneNumber { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
